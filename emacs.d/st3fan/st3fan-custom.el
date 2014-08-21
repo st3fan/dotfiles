cf43adacd66e56ec34aef2614a9245f6b42f183d
Stefan Arentz <stefan@arentz.ca>
2014-08-21 16:10:58 -0400

    Some magit customization

--- emacs.d/st3fan/st3fan-custom.el
+++ emacs.d/st3fan/st3fan-custom.el
@@ -93,3 +93,23 @@
 # This buffer is for notes you don't want to save. If you want to
 # create a file, visit that file with C-x C-f, then enter the text
 # in that file's own buffer.\n")
+
+;; magit
+
+(require 'magit)
+
+(global-set-key (kbd "C-c C-g") 'magit-status)
+
+(defadvice magit-status (around magit-fullscreen activate)
+  "Make magit-status run alone in a frame."
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restore the previous window configuration and kill the magit buffer."
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
