6f4a81003ed1445b5736e9784f6cb1fe2dd5a10a
Stefan Arentz <stefan@arentz.ca>
2014-08-21 21:13:55 -0400

    Remove all bold faces. Change font to Menlo 12 if on Mac.

--- emacs.d/st3fan/st3fan-theme.el
+++ emacs.d/st3fan/st3fan-theme.el
@@ -1,14 +1,26 @@
 ;; st3fan-theme.el
 
+;; This gem removes bold from all faces - http://stackoverflow.com/a/20693389
+
+(defun remap-faces-default-attributes ()
+  (let ((family (face-attribute 'default :family))
+        (height (face-attribute 'default :height)))
+    (mapcar (lambda (face)
+              (face-remap-add-relative
+               face :family family :weight 'normal :height height))
+          (face-list))))
+
+(when (display-graphic-p)
+  (add-hook 'minibuffer-setup-hook 'remap-faces-default-attributes)
+  (add-hook 'change-major-mode-after-body-hook 'remap-faces-default-attributes))
+
 (if (eq window-system 'ns)
     (load-theme 'flatui t)
   (load-theme 'cyberpunk t))
 
 (when (eq window-system 'ns)
-  (set-frame-height (selected-frame) 40)
-  (set-frame-width (selected-frame) 100)
-  (custom-set-faces
-   '(default ((t (:height 140 :width normal :family "Menlo"))))))
-
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;;(load-theme 'hickey t)
+  (when (eq system-type 'darwin)
+    (set-frame-height (selected-frame) 42)
+    (set-frame-width (selected-frame) 120)
+    (custom-set-faces
+     '(default ((t (:height 110 :family "Menlo")))))))
